d0aaa116e78c31c79d381aa1e96c36a32f04e7fb
piru72 <parvezdirom2000@gmail.com>
2022-01-27 11:31:41 +0600

    notes for php

--- test.php
+++ test.php
@@ -1,2 +1,35 @@
 <?php
-    echo "Hello World";
+    // echo "Hello World from PHP!\n";
+    // we are going for variable
+    // $name = "John";
+    // $age = 30;
+    // $isMale = true;
+    // $isMarried = false;
+    // $salary = 1000.50;
+    // echo $age * $age ."\n";
+    // echo $isMale;
+    //echo "\nMy name is {$name}\n";
+    // 1.3 start from here 
+    // $planetName = "Earth";
+    // $personName = "Parvez";
+    // var_dump(strtoupper($planetName), $personName);
+    //define('pi',1.22);
+
+    // $number = 12;
+    // $number += 12;
+    // $number %= 5;
+
+    // echo $number;
+
+    //1.12
+
+    $number_1 = 11 ;
+
+    if ($number_1 % 2 == 0)
+    {
+        echo "$Even";
+    }
+    else 
+    {
+        echo "odd";
+    }
