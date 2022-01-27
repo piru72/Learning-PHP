14c4c402562dc58601c547fdf6be44873cdf8ad5
piru72 <parvezdirom2000@gmail.com>
2022-01-27 12:20:13 +0600

    echo print

---
+++ Yahoo baba/3_print_echo.php
@@ -0,0 +1,14 @@
+<?php
+    echo "Yahoo baba<br>\n";
+    echo "yahoo","baba<br>\n";
+    echo "Yahoo"."baba<br>\n";
+    echo 'Yahoo baba<br>\n'; // so \n doesn't work in single slash 
+    echo "<b> Yahoo Baba</b><br>\n"; // bold tag will execute in html only
+    echo 502.25;
+
+    print "yahoo"."baba<br>\n";
+
+
+    echo "<h1> Yahoo baba </h1>";
+
+?>
